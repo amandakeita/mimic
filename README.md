9b5eaaf3f1b01acae78526001d687cc74f19257c
Amanda Keita Saulīte <amanda@uniso.lv>
2026-06-03 20:47:08 +0300

    buffer and looping

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Linux Device Drivers, Third Edition - ch1,3
 https://kernel.org/doc/html/latest/userspace-api/media/v4l/open.html
 https://kernel.org/doc/html/latest/userspace-api/media/v4l/capture.c.html
 https://youtu.be/0me3guauqOU?si=z--OMwF9AG-MA0ll
+https://www.kernel.org/doc/html/v4.9/media/uapi/v4l/vidioc-querycap.html
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int openCam(int fd){
 int machineCheck(int fd){
     v4l2_capability capability;
     if(ioctl(fd,    VIDIOC_QUERYCAP, &capability)<0){
-        cout<<" failed to understand device "<<VIDIOC_QUERYCAP;
+        perror("failed to understand device, VIDIOC_QUERYCAP");
         return 1;
     }
 }
@@ -41,15 +41,60 @@ int formatSetting(int fd){
     imgFormat.fmt.pix.field = V4L2_FIELD_NONE;
     imgFormat.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     if(ioctl(fd, VIDIOC_S_FMT, &imgFormat)<0){
-        cout<<" unsuccessful format setting "<<VIDIOC_S_FMT;
+        perror("unsuccessful format setting, VIDIOC_S_FMT");
         return 1;
     }
 }
 
 
+int bufferPrep(int fd){
+    v4l2_buffer queryBuffer = {0};
+    queryBuffer.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    queryBuffer.memory = V4L2_MEMORY_MMAP;
+    queryBuffer.index = 0;
+    if(ioctl(fd, VIDIOC_QUERYBUF, &queryBuffer)<0){
+        perror("buffer info wasnt returned");
+        return 1;
+    }
+
+    char* buffer = (char*)mmap(NULL, queryBuffer.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, queryBuffer.m.offset);
+    memset(buffer, 0, queryBuffer.length);
+}
+
+int bufferFrame(int fd){
+    v4l2_buffer bufferInfo;
+    memset(&bufferInfo, 0, sizeof(bufferInfo));
+    bufferInfo.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    bufferInfo.memory = V4L2_MEMORY_MMAP;
+    bufferInfo.index = 0;
+
+    int type = bufferInfo.type;
+    if(ioctl(fd, VIDIOC_STREAMON, &type) < 0){
+        perror("Could not start streaming, VIDIOC_STREAMON");
+        return 1;
+    }
+}
+
+int captureLoop(int fd, int *bufferInfo){
+    if(ioctl(fd, VIDIOC_QBUF, &bufferInfo)<0){
+        perror("failed queuing buffer, VIDIOC_QBUF");
+        return 1;
+    }
+    if(ioctl(fd, VIDIOC_DQBUF, &bufferInfo)<0){
+        perror("failed dequeuing buffer, VIDIOC_DQBUF");
+        return 1;
+    }
+
+    //  !
+    ofstream outFile;
+    outFile.open("webcam_output.jpeg", ios::binary | ios::app);
+}
+
 int main (){
     int fd;
 
+    bufferFrame(fd);
+    bufferPrep(fd);
     formatSetting(fd);
     machineCheck(fd);
     openCam(fd);
